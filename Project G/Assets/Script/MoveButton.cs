0dba64b908ca4eb9a74a5f120a83fc218d3760a0
LeeYonggi <35089435+LeeYonggi@users.noreply.github.com>
2018-05-05 17:57:11 +0900

    prectice

---
+++ Project G/Assets/Script/MoveButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveButton : MonoBehaviour {
+
+    public GameObject gObj;
+    public Transform velocity;
+    public float speed;
+    
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate() 
+    {
+        gObj.transform.position += new Vector3(-speed, 0, 0) * Time.fixedDeltaTime;
+    }
+}
