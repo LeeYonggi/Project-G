9aa81f1e7b09879afc5ca4b03662003c13248e18
LeeYonggi <35089435+LeeYonggi@users.noreply.github.com>
2018-05-06 17:46:45 +0900

    Shader Test

--- Project G/Assets/Script/MoveButton.cs
+++ Project G/Assets/Script/MoveButton.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 public class MoveButton : MonoBehaviour {
 
     public GameObject gObj;
-    public Transform velocity;
+    private Transform m_position;
+    private BoxCollider2D m_bc2D;
     public float speed;
     
 	// Use this for initialization
 	void Start () {
-	    
+        m_position = GetComponent<Transform>();
+        m_bc2D = GetComponent<BoxCollider2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        
+    }
 
     void FixedUpdate() 
     {
-        gObj.transform.position += new Vector3(-speed, 0, 0) * Time.fixedDeltaTime;
+        
+    }
+
+    private void MoveCharacter(float _speed)
+    {
+        gObj.transform.position += new Vector3(_speed, 0, 0) * Time.deltaTime;
+    }
+
+    private void OnMouseOver()
+    {
+        MoveCharacter(speed);
     }
 }
---
+++ Project G/Assets/Script/Shaders/BasicShader.shader
@@ -0,0 +1,21 @@
+﻿Shader "LimePapa/BasicShader"
+{
+	Properties
+	{
+		_Color("Color", Color) = (1, 1, 1, 1)
+	}
+	SubShader
+	{
+		Pass
+		{
+			Material
+			{
+				Diffuse[_Color]
+			}
+			
+			Lighting On
+		}
+	}
+
+	FallBack "Diffuse"
+}
